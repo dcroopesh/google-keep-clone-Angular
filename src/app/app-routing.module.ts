b0e5924b6bfb12a7a39a3281cde89c840cc31216
Roopesh <dcroopesh96@gmail.com>
2020-04-28 09:56:47 +0530

    [Roopesh]Refactor.routing pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { HomeComponent } from './home/home.component';
+import { FlexDesignComponent } from './flex-design/flex-design.component';
+import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
+import { IconsComponent } from './icons/icons.component';
+import { CreateNoteComponent } from './create-note/create-note.component';
 
 
 const routes: Routes = [
@@ -11,7 +16,13 @@ const routes: Routes = [
   { path: '',redirectTo: '/registration',pathMatch:"full"},   
   { path: 'registration' , component: RegistrationComponent },
   { path: 'login' , component: LoginComponent},
-  { path: 'forgot-password' , component: ForgotPasswordComponent},
+  { path: 'forgotpassword' , component: ForgotPasswordComponent},
+  { path: 'home' , component: HomeComponent},
+  { path: 'practice' , component: FlexDesignComponent},
+  { path: 'resetpassword/:token' , component : ResetpasswordComponent},
+  { path: 'icons' , component: IconsComponent},
+  { path: 'cnote' , component: CreateNoteComponent},
+
   { path: '**' , component: PageNotFoundComponent}
   
 ];
