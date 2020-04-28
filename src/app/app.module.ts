42c96a65cea886e230600ecf508f5baf57184114
Roopesh <dcroopesh96@gmail.com>
2020-04-28 09:58:40 +0530

    [Roopesh]Refactor.module file that contains dependent components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,28 @@ import { MatCardModule } from '@angular/material/card';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FlexDesignComponent } from './flex-design/flex-design.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
-
+import {MatIconModule} from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+import { UtilityService } from './services/utility.service';
+import { UserService } from './services/user.service';
+import { HttpService } from './services/http.service';
+import { HomeComponent } from './home/home.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { IconsComponent } from './icons/icons.component';
+import { CreateNoteComponent } from './create-note/create-note.component';
+
+
 
 @NgModule({
   declarations: [
@@ -28,7 +39,11 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     FlexDesignComponent,
     LoginComponent,
     PageNotFoundComponent,
-    ForgotPasswordComponent
+    ForgotPasswordComponent,
+    ResetpasswordComponent,
+    HomeComponent,
+    IconsComponent,
+    CreateNoteComponent
   ],
   imports: [
     BrowserModule,
@@ -43,11 +58,18 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     MatFormFieldModule,
     HttpClientModule, 
     AppRoutingModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatSidenavModule
     
     
   ],
-  providers: [],
+  providers: [
+    HttpService,
+    UtilityService,
+    UserService,
+   
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
