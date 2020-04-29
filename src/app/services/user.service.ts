8a10430291b71b4080b680ae088fa5633c597c46
Roopesh <dcroopesh96@gmail.com>
2020-04-29 11:08:46 +0530

    [Roopesh]Refactor.added http headers  and createnotes,getnotes services

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,50 +2,72 @@ import { Injectable } from '@angular/core';
 import { HttpService } from './http.service';
 import { UtilityService } from './utility.service';
 import { environment } from '../../environments/environment'
+import { HttpHeaders } from '@angular/common/http';
  
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  BaseUserURL;
+  
   
   constructor(private apiRequest : HttpService,private util : UtilityService) {
     
-    this.BaseUserURL = environment.BaseUserURL;
-    console.log(environment.BaseUserURL)
-    console.log(this.BaseUserURL)
+  
   }
 
   registration(dataObject){
 
-    return this.apiRequest.postRequest(this.BaseUserURL + 'userSignUp/',dataObject)
+    return this.apiRequest.postRequest(environment.BaseUserURL + 'userSignUp/',dataObject)
     
   }
 
   login(dataObject){
 
-    return this.apiRequest.postRequest(this.BaseUserURL + 'login/',dataObject)
+    return this.apiRequest.postRequest(environment.BaseUserURL + 'login/',dataObject)
     
   }
 
   forgotPassword(dataObject){
     
-    return this.apiRequest.postRequest(this.BaseUserURL + 'reset/',dataObject)
+    return this.apiRequest.postRequest(environment.BaseUserURL + 'reset/',dataObject)
   }
 
   resetPassword(dataObject){
     
-    // const httpOptions = {
-    //   headers: new HttpHeaders({
-    //     'Authorization': localStorage.getItem("token"),
+    const httpOptions = {
+    
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': localStorage.getItem("token"),
         
-    //   })
-    // }
+      }
+    
+      console.log(this.apiRequest.getEncodData(dataObject))
         
-      return this.apiRequest.postRequest(this.BaseUserURL + 'reset-password/',
-      dataObject,{'Authorization': localStorage.getItem("token")})
+      return this.apiRequest.postRequest(environment.BaseUserURL + 'reset-password/',
+      this.apiRequest.getEncodData(dataObject),httpOptions)
 
+  }
+
+  createNotes(dataObject){
+    const httpOptions = {
+    
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': localStorage.getItem("token"),
+      
+    }
+    return this.apiRequest.postRequest(environment.BaseNotesURL + 'addNotes/',
+    this.apiRequest.getEncodData(dataObject),httpOptions)
+  }
+
+  getNotes(){
+    const httpOptions = {
+    
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': localStorage.getItem("token"),
+      
+    }
+  return this.apiRequest.getRequest(environment.BaseNotesURL + 'getNotesList/',httpOptions)
   }
 
 }
