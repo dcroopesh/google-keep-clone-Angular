d7dafc52366efdf54314ea3703ad44de20fbb9fb
Roopesh <dcroopesh96@gmail.com>
2020-05-01 19:04:33 +0530

    [Roopesh]Refactor.added archive property

--- src/app/create-note/create-note.component.ts
+++ src/app/create-note/create-note.component.ts
@@ -1,41 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, OnChanges, AfterContentInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { FormControl } from '@angular/forms';
 import { MatDivider } from '@angular/material/divider';
+import { MatDialogRef } from '@angular/material/dialog';
+import { UtilityService } from '../services/utility.service';
 
 @Component({
   selector: 'app-create-note',
   templateUrl: './create-note.component.html',
   styleUrls: ['./create-note.component.scss']
 })
-export class CreateNoteComponent implements OnInit {
+export class CreateNoteComponent implements OnInit{
 
   hidden = true;
-  constructor(private requests : UserService) { }
+  noteColour="#ffffff";
   dataObject;
+  archive = false;
+  @Output() onNoteCreate = new EventEmitter<string>();
+
+  
+  constructor(private requests : UserService ,private util : UtilityService) {
+
+   }
+  
   
   ngOnInit(): void {
+
   }
 
-  save(message){
+  ngDoCheck()	
+{
+
+}
+
+  ngAfterContentInit(): void{
+   
+  }
+  
+  getColor(color){
+    this.noteColour = color;
+    document.getElementById("container").style.backgroundColor = this.noteColour;
+  }
+
+
+  save(message?){
 
    this.dataObject =  {
       title: document.getElementById("title").textContent,
       description: document.getElementById("description").textContent,
       isPined: false,
-      color: "#FFFFFF",
-      isArchived: false,
+      color: this.noteColour,
+      isArchived: this.archive,
       labelIdList: [],
       reminder: '',
       collaberators: []
     }
-    this.requests.createNotes(this.dataObject)
+
+    this.noteColour = "#ffffff";
+    this.archive = false;
+
+   this.requests.createNotes(this.dataObject)
     .subscribe((response) =>{
       console.log("Note created")
-      this.hidden = !this.hidden
+      this.hidden = !this.hidden;
+      this.onNoteCreate.emit("note created");
+     
     },
     (error) => {
-      console.log("Didnt created a note")
+      console.log("Didn't create a note")
       this.hidden = !this.hidden
     })
   }
@@ -43,5 +75,12 @@ export class CreateNoteComponent implements OnInit {
   hide(){
     this.hidden = !this.hidden
   }
+
+  Archive(){
+    this.archive = true;
+    this.save();
+  }
+
+  
   
 }
