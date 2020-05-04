e2c220b35a0d1ebad3a4b83db5784ed1cde1634f
Roopesh <dcroopesh96@gmail.com>
2020-05-04 10:02:09 +0530

    [Roopesh]Refactor.added a method get the labels

--- src/app/Icons/more/more.component.ts
+++ src/app/Icons/more/more.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
+import { DataService } from 'src/app/services/data.service';
 
 
 @Component({
@@ -10,23 +11,43 @@ import { UserService } from 'src/app/services/user.service';
 export class MoreComponent implements OnInit {
 
   display = false;
+  show = false;
   @Input() noteId;
   @Output() ondelete = new EventEmitter<string>() ;
-  
-  constructor( private requests: UserService ) { }
-  
+  @Input() newNote;
+
+  checked = [];
+
+  @Output() onLabelSelect = new EventEmitter<any>() ;
+
+  labels;
+  labelArray = [];
+  constructor( private requests: UserService , private data : DataService) { 
+    
+    this.data.label.subscribe(next =>{
+      this.labels = next;
+      console.log("more")
+      console.log(this.labels)
+    })
+
+    for(let i=0; i< this.labels.length;i++){
+      this.checked.push(false) ;  
+    }
+
+   }
 
   ngOnInit(): void {
   }
 
   displayLabel(){
     this.display = ! this.display;
+    this.show=false;
+  
   }
 
   deleteNote(){
     
-    console.log(this.noteId);
-    
+   console.log(this.noteId);
     let dataObject = {
       isDeleted : true,
       noteIdList : [this.noteId]
@@ -44,5 +65,24 @@ export class MoreComponent implements OnInit {
 
       })
 
+  }
+  showLabels(){
+    this.show= !this.show;
+    
+  }
+
+  selectLabel(label,i){  
+    
+    this.checked[i] = ! this.checked[i];
+    if(this.checked[i]){
+    this.labelArray.push(label) 
+    }
+    else{
+      for(var j=0 ; j < this.labelArray.length;j++){
+        if (label == this.labelArray[j])
+          this.labelArray.splice(j,1)
+      }
+    }
+    this.onLabelSelect.emit(this.labelArray);
   }
 }
