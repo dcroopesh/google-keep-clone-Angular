e211d111e257881914f2a9bae368855b7ba7fd1b
Roopesh <dcroopesh96@gmail.com>
2020-05-11 17:28:45 +0530

    [Roopesh]Refactor.added functionality for all the icons

--- src/app/display-note/display-note.component.ts
+++ src/app/display-note/display-note.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, Input, Output, EventEmitter, AfterContentInit, DoCheck } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, AfterContentInit, DoCheck, OnChanges } from '@angular/core';
 import { UserService } from '../services/user.service';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { CreateNoteComponent } from '../create-note/create-note.component';
 import { UpdateNotesComponent } from '../update-notes/update-notes.component';
+import { CollaboratorComponent } from '../Icons/collaborator/collaborator.component';
+import { CollabDialogComponent } from '../collab-dialog/collab-dialog.component';
 
 
 @Component({
@@ -10,35 +12,35 @@ import { UpdateNotesComponent } from '../update-notes/update-notes.component';
   templateUrl: './display-note.component.html',
   styleUrls: ['./display-note.component.scss']
 })
-export class DisplayNoteComponent implements OnInit,DoCheck {
+export class DisplayNoteComponent implements OnInit,OnChanges{
 
   @Input() notesObjects;
   show = false;
-  @Output() onDelete = new EventEmitter<string>();
+  @Output() notifyNote = new EventEmitter();
   @Output() onUNArchive = new EventEmitter<string>();
   newNote="True"
   noteId;
   remainderArray = [];
   displayDate = [];
-
+  collaborators = {};
   month = ['Jan','Feb','March','Apr','May','June','July','Aug','Sep','Oct','Nov','Dec']
 
-  constructor(private requests : UserService,public dialog: MatDialog) {
-    console.log("abc",this.notesObjects);
-
-   }
+  constructor(private requests : UserService,public dialog: MatDialog) {}
 
   ngOnInit(): void {};
   
-  
-
-  ngDoCheck()	{
+  ngOnChanges()	{
     var n = this.notesObjects;
+    this.collaborators = []
+    this.displayDate = [];
       for(let i = 0; i < n.length;i++){
+
+      //to send data to colab dialog  
+      this.collaborators[n[i]['id']] = n[i]['collaborators'];
 
       let dateUTC = n[i]['reminder'][0]
           if (dateUTC != undefined){
-            let dateIST = new Date(dateUTC);
+            let dateIST = new Date(dateUTC)    ;
 
       this.displayDate[i]= ' ' +  this.month[dateIST.getMonth()] + ' ';
       this.displayDate[i] += dateIST.getDate() + ' ';
@@ -76,18 +78,117 @@ export class DisplayNoteComponent implements OnInit,DoCheck {
     this.noteId = item.id;
   }
 
-  send(msg){
-    this.onDelete.emit("deleted")
+  send(msg?){
+    this.notifyNote.emit()
   }
 
   unarchive(){
     this.onUNArchive.emit();
   }
 
+  updateColor(colour){
+    console.log(colour)
+
+  let dataObject = {
+    color : colour,
+    noteIdList : [this.noteId],
+
+  }
+    this.requests.changesColorNotes(dataObject)
+    .subscribe((response) => {
+      this.send();
+    },(error) => {
+      console.log(error)
+    })
+  }
+
+  archiveNotes(){
+    
+    let dataObject = {
+      isArchived : true,
+      noteIdList : [this.noteId]
+    }
+
+    this.requests.archiveNotes(dataObject)
+    .subscribe((response) => {
+      this.send();
+    },(error) => {
+      console.log(error)
+    })
+
+    
 
 
-  
-  
-  
+  }
 
+  addUpdateReminderNotes(date){
+    
+  let dataObject = {
+    reminder : date,
+    noteIdList : [this.noteId]
+  }
+
+  this.requests.addUpdateReminderNotes(dataObject)
+  .subscribe((response) => {
+    this.send();
+  },(error) => {
+    console.log(error)
+  })
+  }
+
+  addLabel(labelArray){
+    console.log("add","")
+    let labelId = labelArray[0].id;
+
+    this.requests.add(labelId,this.noteId)
+    .subscribe((response) =>{
+      this.send();
+
+    },
+    (error) => {
+      console.log(error)
+
+    })
+  }
+
+  removeLabel(label){
+
+    this.requests.remove(label.id,this.noteId)
+    .subscribe((response) =>{
+      this.send();
+
+    },
+    (error) => {
+      console.log(error)
+
+    })
+  }
+
+  removeReminderNotes(){
+    let id = []
+    id.push(this.noteId)
+    let dataObject = {
+      noteIdList : id
+    }
+    this.requests.removeReminderNotes(dataObject)
+    .subscribe((response) =>{
+      this.send();
+
+    },
+    (error) => {
+      console.log(error)
+
+    })
+  }
+
+  showColabDialog(){
+
+
+      const dialogRef = this.dialog.open(CollabDialogComponent, {
+      width: '450px',
+      data: {item: this.noteId ,collab : this.collaborators[this.noteId]}
+      });
+
+  }
+  
 }
