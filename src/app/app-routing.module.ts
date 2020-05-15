879f3d139d603cefa5ff60ac92c9da70eeb7e89e
Roopesh <dcroopesh96@gmail.com>
2020-05-15 11:26:52 +0530

    [Roopesh]Refactor.added question and answer component to the route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,8 @@ import { DisplayNoteComponent } from './display-note/display-note.component';
 import { ArchiveComponent } from './Icons/archive/archive.component';
 import { ArchiveSidenavComponent } from './archive-sidenav/archive-sidenav.component';
 import { LabelsSidenavComponent } from './labels-sidenav/labels-sidenav.component';
+import { AuthGuard } from './auth.guard';
+import { QuesAnsComponent } from './ques-ans/ques-ans.component';
 
 
 const routes: Routes = [
@@ -23,17 +25,14 @@ const routes: Routes = [
   { path: 'registration' , component: RegistrationComponent },
   { path: 'login' , component: LoginComponent},
   { path: 'forgotpassword' , component: ForgotPasswordComponent},
-  { path: 'display' , component: DisplayNoteComponent},
 
-  { path: 'practice' , component: FlexDesignComponent},
   { path: 'resetpassword/:token' , component : ResetpasswordComponent},
-  { path: 'icons' , component: IconsComponent},
-  { path: 'cnote' , component: CreateNoteComponent},
-  { path: 'home' , component: HomeComponent ,
+  { path: 'home' , component: HomeComponent , canActivate : [AuthGuard],
     children : [{path : '' , component : NotesComponent},
                 {path : 'remainder' , component : RemaindersComponent},
                 { path: 'archive' , component: ArchiveSidenavComponent},
-                { path: 'label/:labelname' , component: LabelsSidenavComponent}, ]
+                { path: 'label/:labelname' , component: LabelsSidenavComponent},
+                { path: 'questionAnswer/:noteId' , component: QuesAnsComponent} ]
               
  },
   { path: '**' , component: PageNotFoundComponent},
