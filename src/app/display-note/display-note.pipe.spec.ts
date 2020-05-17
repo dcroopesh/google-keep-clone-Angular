3daff2ebf63029572c1d91aba485f7a5e33d9eb4
Roopesh <dcroopesh96@gmail.com>
2020-05-17 16:49:50 +0530

    [Roopesh]Refactor.unit test sript

---
+++ src/app/display-note/display-note.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DisplayNotePipe } from './display-note.pipe';
+
+describe('DisplayNotePipe', () => {
+  it('create an instance', () => {
+    const pipe = new DisplayNotePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
