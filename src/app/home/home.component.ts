77f9d06a688258246cdd164d939356ea08e4f0d9
Roopesh <dcroopesh96@gmail.com>
2020-05-17 16:43:11 +0530

    [Roopesh]Refactor.implemented image dialog and logout api

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,28 +4,55 @@ import { LabelsComponent } from '../labels/labels.component';
 import { MatDialog } from '@angular/material/dialog';
 import { UserService } from '../services/user.service';
 import { DataService } from '../services/data.service';
+import { environment } from '../../environments/environment'
+import { ProfilePicComponent } from '../profile-pic/profile-pic.component';
+import { FormControl } from '@angular/forms';
+
+
+class ImageSnippet {
+  constructor(public src: string, public file: File) {}
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
+
+
 export class HomeComponent implements OnInit {
 
   labels = [] ;
   signOut=false;
+  firstName;
+  lastName;
+  email;
+  selectedFile: ImageSnippet;
+  profilePic;
+  grid=true
+  imageData
+  searchText = new FormControl()
   
   constructor(private route: Router,public dialog: MatDialog,private requests: UserService,
     private data : DataService) { 
-   console.log("homescons")
     this.getLabels(); 
-  }
+    this.firstName = localStorage.getItem('firstName');
+    this.lastName = localStorage.getItem('lastName');
+    this.email = localStorage.getItem('email');
+    this.profilePic = environment.BaseURl + localStorage.getItem('userImage');
+
+}
 
   ngOnInit(): void {
   }
 
   dispalySignOut(){
     this.signOut = ! this.signOut;
+  }
+
+  sendData(){
+    this.data.searchNote(this.searchText)
   }
 
  showNotes(){
@@ -47,7 +74,6 @@ showLabels(labelname){
 getLabels(){
   this.requests.getNoteLabelList()
     .subscribe((response) =>{
-    console.log("subs")
     this.labels = response['data']['details'];
     this.data.changeLabel(this.labels)
   })
@@ -58,5 +84,40 @@ showDialog(){
 
     const dialogRef = this.dialog.open(LabelsComponent, {width: '300px',
     data: {label: this.labels}
-})}
-    }
+    })
+  }
+
+  uploadImageDialog(){
+    
+      // document.getElementById("FileUpload").click();
+      console.log("asd")
+      const dialogRef = this.dialog.open(ProfilePicComponent, {width: '400px',height :"450px"})
+      
+    
+  }
+
+  logout(){
+    this.requests.logout()
+    .subscribe((response)=>{
+      localStorage.clear();
+      this.route.navigate(['/login'])
+
+    },
+    (error)=>{
+      console.log(error);
+    })
+  }
+
+
+  showGridOrList(){
+    
+    this.grid = ! this.grid
+    this.data.changeView(! this.grid)
+  }
+
+
+
+  
+}
+
+  
